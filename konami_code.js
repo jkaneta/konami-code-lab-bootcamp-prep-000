c7cf5fc8f42bb5fbbd2ce6ca2e7b25de260dd562
J <flatironschool@2joyce.com>
2018-01-18 06:20:27 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -8,7 +8,7 @@ function init() {
   
   document.body.addEventListener('keydown', function(e) {
     let keyPressed = e.code;
-    console.log(keyPressed, e, e.which, e.detail, e.location);
+    console.log(keyPressed, e.which, e.detail, e.location);
     if (keyPressed === updatedCode[codeCount]) {
       codeCount++;
       
