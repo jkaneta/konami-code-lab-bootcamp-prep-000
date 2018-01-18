a725846bace57caf3972eb13835e92348f332ee2
J <flatironschool@2joyce.com>
2018-01-18 05:45:59 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+const updatedCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'KeyA', 'KeyB'];
+
 function init() {
   // Write your JavaScript code inside the init() function
   
